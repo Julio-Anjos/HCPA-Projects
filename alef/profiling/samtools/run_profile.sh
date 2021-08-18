edb284dd1117a9c3f48e9487281853fd327c01e5
Alef Farah <afarah@inf.ufrgs.br>
2021-08-17 21:06:41 -0300

    support index and improve script

--- alef/profiling/samtools/run_profile.sh
+++ alef/profiling/samtools/run_profile.sh
@@ -4,8 +4,6 @@
 set -e
 set -o pipefail
 
-echo "Expected input: ./run_profile.sh steps cram ref"
-
 # read vars
 origin="$(pwd)"
 samtools_folder_path="$(pwd)/../../samtools"
@@ -20,16 +18,21 @@ case $run in
   ;;
 
   "view")
+    echo "Expected input: ./run_profile.sh view cram ref"
     f_input="view_input"
     f_validate="view_validate"
-    f_build="view_build"
-    f_setup="view_setup"
+    f_build="samtools_build"
+    f_setup="common_setup"
     f_profile="view_profile"
   ;;
 
   "index" )
-  echo "Unimplemented."
-  exit 1
+    echo "Expected input: ./run_profile.sh index bam"
+    f_input="index_input"
+    f_validate="index_validate"
+    f_build="samtools_build"
+    f_setup="common_setup"
+    f_profile="index_profile"
   ;;
 
   *)
@@ -37,6 +40,30 @@ case $run in
   exit 1
   ;;
 esac
+
+#
+# Common
+#
+
+function samtools_build {
+  cd "$samtools_folder_path"
+  git --no-pager log --pretty=oneline --max-count=1
+  git stash
+  make clean
+  make -j6
+}
+
+function common_setup {
+  # env setup
+  cd "$origin"
+  profile_path="$(pwd)/$1_$(date +'%d_%m_%y_%H%M%S')"
+  mkdir $profile_path
+  cd $profile_path
+}
+
+#
+# View
+#
 
 function view_input {
   cram="$2"
@@ -51,26 +78,31 @@ function view_validate {
   ls -l "$ref"
 }
 
-function view_build {
-  cd "$samtools_folder_path"
-  git --no-pager log --pretty=oneline --max-count=1
-  git stash
-  make clean
-  make -j6
-}
-
-function view_setup {
-  # env setup
-  cd "$origin"
-  profile_path="$(pwd)/$(date +'%d_%m_%y_%H%M%S')"
-  mkdir $profile_path
-  cd $profile_path
-}
-
 function view_profile {
   # total time profiling
   time_start=$(date +%s%3N)
   "$samtools" view -b -T "$ref" "$cram" >out.bam
+  time_end=$(date +%s%3N)
+  echo "total_time_ms=$((time_end - time_start))"
+}
+
+#
+# Index
+#
+
+function index_input {
+  bam="$2"
+}
+
+function index_validate {
+  ls -d "$samtools_folder_path"
+  test -n "$bam"
+  ls -l "$bam"
+}
+
+function index_profile {
+  time_start=$(date +%s%3N)
+  "$samtools" index "$bam" 
   time_end=$(date +%s%3N)
   echo "total_time_ms=$((time_end - time_start))"
 }
@@ -82,7 +114,7 @@ $f_validate
 echo "Build samtools..."
 $f_build
 echo "Setup run env..."
-$f_setup
+$f_setup "$1"
 echo "Profiling... It is $(date)"
 $f_profile
 echo "Done... It is $(date)" 
