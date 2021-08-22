3f20c19d0c375e7c278a2721198f56bc9e5bae75
Alef Farah <afarah@inf.ufrgs.br>
2021-08-22 17:11:06 -0300

    par vs seq

--- alef/profiling/samtools/run_pipeline.sh
+++ alef/profiling/samtools/run_pipeline.sh
@@ -16,28 +16,38 @@ ls -d "$samtools_folder_path"
 test -n "$runtype"
 test -n "$cram_list"
 ls -l "$cram_list"
+cat "$cram_list" | xargs ls -l
 
-# Function to run in parallel
-do_foreach_cram() {
-  cram="$1"
-  echo "Indexing $cram at $(date)..."
-  "$samtools" index "$cram" 
-  echo "Finished indexing $cram at $(date)..."
-  index="$cram.crai"
-  cram_name="$(basename -s $cram)"
-  
-  echo "Viewing regions for $cram at $(date)"
+function do_foreach_region_seq {
   for region in $(seq 1 22)
   do
     "$samtools" view -C -X "$cram" "$index" "chr$region" >"out_region${region}_${cram_name}.bam"
   done
+}
+
+function do_foreach_region_par {
+  seq 1 23 | parallel
+    "$samtools" view -C -X "$cram" "$index" "chr{}" >"out_region{}_${cram_name}.bam"
+}
+
+function do_foreach_cram {
+  cram="$1"
+  doregion="$2"
+  echo "Indexing $cram at $(date)..."
+  "$samtools" index "$cram" 
+  echo "Finished indexing $cram at $(date)..."
+  index="$cram.crai"
+  cram_name="${cram##*/}"
+  cram_name="${cram_name%.*}"
+  echo "Viewing regions for $cram at $(date)"
+  $doregion 
   echo "Finished viewing regions for $cram at $(date)"
 }
 
 # Start
 
 samtools_build
-common_setup "pipeline_$run"
+common_setup "pipeline_$runtype"
 
 echo "Starting at $(date)..."
 if test "$runtype" = "seq"
@@ -45,15 +55,19 @@ then
   time_start=$(date +%s%3N)
   for cram in $(cat "$cram_list")
   do
-    do_foreach_cram "$cram"
+    do_foreach_cram "$cram" "do_foreach_region_seq"
   done
   time_end=$(date +%s%3N)
   echo "total_time=$((time_end - time_start))"
-else if test "$runtype" = "par" then
+elif test "$runtype" = "par" 
+then
   export SHELL=$(type -p bash)
   export -f do_foreach_cram
-  cat "$cram_list" | parallel do_foreach_cram
+  #export -f do_foreach_region_par
+  export -f do_foreach_region_seq
+  export samtools=$samtools
+  cat "$cram_list" | parallel do_foreach_cram {} "do_foreach_region_seq"
 else
-  echo "Unimplemented..."
+  echo "Unimplemented... Expecting par or seq"
 fi
 echo "Ending at $(date)..."
