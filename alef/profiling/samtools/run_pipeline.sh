f0636b5ded67f021493d26be6dc341dc22a8a985
Alef Farah <afarah@inf.ufrgs.br>
2021-08-23 19:11:58 -0300

    update draft to usable script

--- alef/profiling/samtools/run_pipeline.sh
+++ alef/profiling/samtools/run_pipeline.sh
@@ -21,7 +21,7 @@ cat "$cram_list" | xargs ls -l
 function do_foreach_cram {
   cram="$1"
   echo "Indexing $cram at $(date)..."
-  "$samtools" index "$cram" 
+  "$samtools" index "$cram"
   echo "Finished indexing $cram at $(date)..."
   index="$cram.crai"
   cram_name="${cram##*/}"
@@ -31,13 +31,13 @@ function do_foreach_cram {
   do
     outname="out_region${region}_${cram_name}.bam"
     "$samtools" view -C -X "$cram" "$index" "chr$region" >"$outname"
-    echo "$outname">>region_list.txt
+    echo "$(pwd)/$outname">>region_list.txt
   done
   echo "Finished viewing regions for $cram at $(date)"
 }
 
 function do_foreach_region { # Region filelist
-  argv="$(grep region_list.txt -e region_$1 | sed 's/\n/ /g')"
+  argv="$(grep region_list.txt -e region$1_ | perl -pe 's/\n/ /g')"
   "$samtools" merge "$argv">merged_region$1.bam
   "$samtools" index merged_region$1.bam
 }
@@ -54,18 +54,20 @@ then
   rm -f region_list.txt
   for cram in $(cat "$cram_list")
   do
-    do_foreach_cram "$cram" 
+    do_foreach_cram "$cram"
   done
-  for i in $(seq 1 22) do
+  for i in $(seq 1 22)
+  do
     do_foreach_region $i
   done
   time_end=$(date +%s%3N)
   echo "total_time=$((time_end - time_start))"
-elif test "$runtype" = "par" 
+elif test "$runtype" = "par"
 then
   rm -f region_list.txt
   export SHELL=$(type -p bash)
   export -f do_foreach_cram
+  export -f do_foreach_region
   export samtools=$samtools
   cat "$cram_list" | parallel do_foreach_cram {}
   seq 1 22 | parallel do_foreach_region {}
