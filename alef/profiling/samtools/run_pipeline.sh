0fe64d89219355635a106a9d87a4f6e98f163ddf
Alef Farah <afarah@inf.ufrgs.br>
2021-08-23 18:16:19 -0300

    draft

--- alef/profiling/samtools/run_pipeline.sh
+++ alef/profiling/samtools/run_pipeline.sh
@@ -18,21 +18,8 @@ test -n "$cram_list"
 ls -l "$cram_list"
 cat "$cram_list" | xargs ls -l
 
-function do_foreach_region_seq {
-  for region in $(seq 1 22)
-  do
-    "$samtools" view -C -X "$cram" "$index" "chr$region" >"out_region${region}_${cram_name}.bam"
-  done
-}
-
-function do_foreach_region_par {
-  seq 1 23 | parallel
-    "$samtools" view -C -X "$cram" "$index" "chr{}" >"out_region{}_${cram_name}.bam"
-}
-
 function do_foreach_cram {
   cram="$1"
-  doregion="$2"
   echo "Indexing $cram at $(date)..."
   "$samtools" index "$cram" 
   echo "Finished indexing $cram at $(date)..."
@@ -40,8 +27,19 @@ function do_foreach_cram {
   cram_name="${cram##*/}"
   cram_name="${cram_name%.*}"
   echo "Viewing regions for $cram at $(date)"
-  $doregion 
+  for region in $(seq 1 22)
+  do
+    outname="out_region${region}_${cram_name}.bam"
+    "$samtools" view -C -X "$cram" "$index" "chr$region" >"$outname"
+    echo "$outname">>region_list.txt
+  done
   echo "Finished viewing regions for $cram at $(date)"
+}
+
+function do_foreach_region { # Region filelist
+  argv="$(grep region_list.txt -e region_$1 | sed 's/\n/ /g')"
+  "$samtools" merge "$argv">merged_region$1.bam
+  "$samtools" index merged_region$1.bam
 }
 
 # Start
@@ -53,20 +51,24 @@ echo "Starting at $(date)..."
 if test "$runtype" = "seq"
 then
   time_start=$(date +%s%3N)
+  rm -f region_list.txt
   for cram in $(cat "$cram_list")
   do
-    do_foreach_cram "$cram" "do_foreach_region_seq"
+    do_foreach_cram "$cram" 
+  done
+  for i in $(seq 1 22) do
+    do_foreach_region $i
   done
   time_end=$(date +%s%3N)
   echo "total_time=$((time_end - time_start))"
 elif test "$runtype" = "par" 
 then
+  rm -f region_list.txt
   export SHELL=$(type -p bash)
   export -f do_foreach_cram
-  #export -f do_foreach_region_par
-  export -f do_foreach_region_seq
   export samtools=$samtools
-  cat "$cram_list" | parallel do_foreach_cram {} "do_foreach_region_seq"
+  cat "$cram_list" | parallel do_foreach_cram {}
+  seq 1 22 | parallel do_foreach_region {}
 else
   echo "Unimplemented... Expecting par or seq"
 fi
