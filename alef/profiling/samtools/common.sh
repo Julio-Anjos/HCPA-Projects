19a192f49c6c750386b826bfb24bd0faa67d6775
Alef Farah <afarah@inf.ufrgs.br>
2021-08-31 19:25:54 -0300

    start bcftools steps

--- alef/profiling/samtools/common.sh
+++ alef/profiling/samtools/common.sh
@@ -3,11 +3,25 @@
 #
 
 origin="$(pwd)"
+
 samtools_folder_path="$(pwd)/../../samtools"
-samtools="$(pwd)/../../samtools/samtools"
+samtools="$samtools_folder_path/samtools"
+
+bcftools_folder_path="$(pwd)/../../bcftools"
+bcftools="$bcftools_folder_path/bcftools"
+
+vcfutils="$bcftools_folder_path/misc/vcfutils.pl"
 
 function samtools_build {
   cd "$samtools_folder_path"
+  git --no-pager log --pretty=oneline --max-count=1
+  git stash
+  make clean
+  make -j6
+}
+
+function bcftools_build {
+  cd "$bcftools_folder_path"
   git --no-pager log --pretty=oneline --max-count=1
   git stash
   make clean
--- alef/profiling/samtools/run_pipeline.sh
+++ alef/profiling/samtools/run_pipeline.sh
@@ -1,31 +1,63 @@
 #!/bin/bash
 
+#
+# Stage 1 - Index and view regions for each CRAM
+# 
+# FOR CRAM_J IN LIST DO PARALLEL
+#   INDEX CRAM_J
+#   FOR REGION_I IN 1,22 DO
+#     VIEW CRAM_J REGION_I >CJ_RI
+#
+# Stage 2 - Merge CRAMs and generate VCFs for each region
+#
+# FOR REGION_I IN 1,22 DO PARALLEL
+#   MERGE (CJ_RI FOR J IN LIST)>MERGED_I
+#   INDEX MERGED_I
+#   MPILEUP
+#   CALL
+#   BCF VIEW
+
+#
+# Setup
+#
+
 # Abort on error
 set -e
 set -o pipefail
 
 source common.sh
 
-# Input
-echo "Expected input: runtype cram_list"
+# Get input
+echo "Expected input: runtype cram_list ref"
 runtype="$1"
 cram_list="$2"
+ref="$3"
 
-# Validate
+# Validate input
 ls -d "$samtools_folder_path"
 test -n "$runtype"
 test -n "$cram_list"
+test -n "$ref"
 ls -l "$cram_list"
 cat "$cram_list" | xargs ls -l
+ls -l "$ref"
 
-function do_foreach_cram {
+#
+# Stage 1 decl
+#
+function do_foreach_cram { # $1=Cram
+  # Index
   cram="$1"
   echo "Indexing $cram at $(date)..."
   "$samtools" index "$cram"
   echo "Finished indexing $cram at $(date)..."
+
+  # Setup
   index="$cram.crai"
   cram_name="${cram##*/}"
   cram_name="${cram_name%.*}"
+
+  # View regions
   echo "Viewing regions for $cram at $(date)"
   for region in $(seq 1 22)
   do
@@ -36,41 +68,75 @@ function do_foreach_cram {
   echo "Finished viewing regions for $cram at $(date)"
 }
 
-function do_foreach_region { # Region filelist
+#
+# Stage 2 decl
+#
+function do_foreach_region { # $1=Region 
   argv="$(grep region_list.txt -e region$1_ | perl -pe 's/\n/ /g')"
   "$samtools" merge "$argv">merged_region$1.bam
   "$samtools" index merged_region$1.bam
+  "$bcftools" mpileup -Ou -o merged_region$1.bcf -f $ref merged_region$1.bam
+  "$bcftools" call -m -u -o call_merged_region$1.bcf merged_region$1.bcf
+  "$bcftools" view call_merged_region$1.bcf | "$vcfutils" "varFilter - > final_region$1.vcf"
 }
 
-# Start
-
+# More setup
 samtools_build
+bcftools_build
 common_setup "pipeline_$runtype"
 
+#
+# Actually running stuff
+#
 echo "Starting at $(date)..."
 if test "$runtype" = "seq"
 then
+  #
+  # Sequential run 
+  # 
+
+  # Setup
   time_start=$(date +%s%3N)
   rm -f region_list.txt
+
+  # Stage 1
   for cram in $(cat "$cram_list")
   do
     do_foreach_cram "$cram"
   done
+
+  # Stage 2
   for i in $(seq 1 22)
   do
     do_foreach_region $i
   done
+
+  # Report results
   time_end=$(date +%s%3N)
   echo "total_time=$((time_end - time_start))"
 elif test "$runtype" = "par"
 then
+  #
+  # Parallel run
+  #
+
+  # Setup
+  time_start=$(date +%s%3N)
   rm -f region_list.txt
   export SHELL=$(type -p bash)
   export -f do_foreach_cram
   export -f do_foreach_region
   export samtools=$samtools
+
+  # Stage 1
   cat "$cram_list" | parallel do_foreach_cram {}
+
+  # Stage 2
   seq 1 22 | parallel do_foreach_region {}
+
+  # Report results
+  time_end=$(date +%s%3N)
+  echo "total_time=$((time_end - time_start))"
 else
   echo "Unimplemented... Expecting par or seq"
 fi
